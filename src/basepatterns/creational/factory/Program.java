c53edc0da848fd52992ebf8a6134fc66f2cc4557
Astrovar <ast@var>
2018-09-19 20:32:21 +0300

    minor modifications

--- src/basepatterns/creational/factory/Program.java
+++ src/basepatterns/creational/factory/Program.java
@@ -13,30 +13,35 @@ import basepatterns.creational.factory.Factories.*;
 
 class Program {
 
-	public static void main(String[] args) {
-		try (Scanner scanner = new Scanner(System.in)) {
-			while (true) {
-				try {
-					System.out.println();
-					System.out.println("Please, enter what language developer you need:");
-					String language = scanner.next();
-					DevelopersFactory developersFactory;
-					developersFactory = createDeveloperBySpeciality(language);
-					Developer developer = developersFactory.createDeveloper();
-					developer.writeCode();
-				} catch (ExceptionUnknownSpeciality e) {
-				}
-			}
-		}
-	}
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            boolean bool = true;
+            while (bool) {
+                try {
+                    System.out.println();
+                    System.out.println("Please, enter what language developer you need:");
+                    String language = scanner.next();
+                    if (!language.equalsIgnoreCase("exit")) {
+                        DevelopersFactory developersFactory;
+                        developersFactory = createDeveloperBySpeciality(language);
+                        Developer developer = developersFactory.createDeveloper();
+                        developer.writeCode();
+                    } else {
+                        bool = false;
+                    }
+                } catch (ExceptionUnknownSpeciality e) {
+                }
+            }
+        }
+    }
 
-	private static DevelopersFactory createDeveloperBySpeciality(String speciality) throws ExceptionUnknownSpeciality {
-		if (speciality.equalsIgnoreCase("java")) {
-			return new JavaDeveloperFactory();
-		} else if (speciality.equalsIgnoreCase("c++")) {
-			return new CppDeveloperFactory();
-		} else {
-			throw new ExceptionUnknownSpeciality(speciality);
-		}
-	}
+    private static DevelopersFactory createDeveloperBySpeciality(String speciality) throws ExceptionUnknownSpeciality {
+        if (speciality.equalsIgnoreCase("java")) {
+            return new JavaDeveloperFactory();
+        } else if (speciality.equalsIgnoreCase("c++")) {
+            return new CppDeveloperFactory();
+        } else {
+            throw new ExceptionUnknownSpeciality(speciality);
+        }
+    }
 }
